8775472d58f5cb13cffbbc3f985a5822c93d3fea
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-17 14:59:29 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      
+      <h1 className='text-center text-blue-500 font-bold text-6xl'>App</h1>
+      
+      </div>
+  )
+}
+
+export default App
